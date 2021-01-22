f2997fa43353ebb321cf36bc6496c7da69eabb67
Pavel Lezin <pavellezin@yandex.ru>
2021-01-22 16:16:35 +0500

    role_auth

--- src/main/java/pro/paullezin/menuvoting/MenuVotingApplication.java
+++ src/main/java/pro/paullezin/menuvoting/MenuVotingApplication.java
@@ -1,24 +1,15 @@
 package pro.paullezin.menuvoting;
 
 import lombok.AllArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pro.paullezin.menuvoting.repository.UserRepository;
 
 @SpringBootApplication
 @AllArgsConstructor
-public class MenuVotingApplication implements ApplicationRunner {
-    private final UserRepository userRepository;
+public class MenuVotingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MenuVotingApplication.class, args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) {
-        System.out.println(userRepository.findByLastNameContainingIgnoreCase("last"));
     }
 
 }
