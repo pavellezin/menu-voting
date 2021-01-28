f96c8dd6c15ee3ae1ce91bb3192cd6004b6c709b
Pavel Lezin <pavellezin@yandex.ru>
2021-01-28 11:48:34 +0500

    refactor_search_by_menu_method_add_uri

--- src/main/java/pro/paullezin/menuvoting/repository/LunchRepository.java
+++ src/main/java/pro/paullezin/menuvoting/repository/LunchRepository.java
@@ -8,6 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 import pro.paullezin.menuvoting.model.Lunch;
+import pro.paullezin.menuvoting.model.Menu;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,9 +20,9 @@ public interface LunchRepository extends JpaRepository<Lunch, Integer> {
     @Query("SELECT l FROM Lunch l WHERE l.menu.date=:date")
     List<Lunch> findByDate(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date);
 
-    @RestResource(rel = "by-menu-id", path = "by-menu-id")
-    @Query("SELECT l FROM Lunch l WHERE l.menu.id=:id")
-    List<Lunch> findByMenuId(Integer id);
+    @RestResource(rel = "by-menu-id", path = "by-menu")
+    @Query("SELECT l FROM Lunch l WHERE l.menu=:menu")
+    List<Lunch> findByMenu(Menu menu);
 
     @Override
     @Transactional
